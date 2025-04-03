4e86ca876dd76d667fce949de01a4c7d26517a81
Mike <mikerockmusic51@gmail.com>
2025-04-03 15:16:45 +0100

    refactor(user): disable unique constraint on phoneNumber in User schema

--- app/models/User.ts
+++ app/models/User.ts
@@ -58,7 +58,7 @@ const UserSchema = new Schema<IUser>(
     phoneNumber: {
       type: String,
       trim: true,
-      unique: true,
+      // unique: true,
     },
     bio: {
       type: String,
